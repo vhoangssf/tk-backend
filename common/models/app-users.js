b64ae21658fa908024fe865773d14156a31926e7
jbrownssf <jpbrown@softstackfactory.org>
2017-04-25 04:30:42 +0000

    added login functions

--- common/models/app-users.js
+++ common/models/app-users.js
@@ -1,5 +1,15 @@
 'use strict';
 
 module.exports = function(Appusers) {
-
+     Appusers.observe('after save', function(ctx, next) {
+       if(ctx.isNewInstance === true) {
+           var instance = ctx.instance;
+           instance.createAccessToken(1209600000, function(err, response) { 
+               if(err === null) ctx.instance["token"] = response.id;
+               next();
+           });
+       }else {
+            next();
+       }
+   });
 };
